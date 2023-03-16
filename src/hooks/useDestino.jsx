a0b0ca344056a8e401843dc7d7a69ae7e8d2c453
Diego Aldao <diego_ind@live.com.ar>
2023-03-16 19:40:20 -0300

    añadido hook para formatear los links de la api

---
+++ src/hooks/useDestino.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useDestino = (link) => {
+  const [linkFormateado, setLinkFormateado] = useState("");
+
+  useEffect(() => {
+    if (link.includes("/")) {
+      let ruta = link.replaceAll("https://www.asos.com/es/", "/").split("?", 2);
+      let url = ruta[0];
+      let querys = ruta[1].replaceAll("cid=", "categoryId=");
+      const destino = `/productos${url}${querys}`;
+      setLinkFormateado(destino);
+    }
+  }, [link]);
+  return { linkFormateado };
+};
+
+export default useDestino;
