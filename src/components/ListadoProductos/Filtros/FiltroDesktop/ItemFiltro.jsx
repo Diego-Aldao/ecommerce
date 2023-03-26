41dab66006279046305397dd79c47118c64d79f0
Diego Aldao <diego_ind@live.com.ar>
2023-03-26 18:11:57 -0300

    añadido el item filtro para desktop

---
+++ src/components/ListadoProductos/Filtros/FiltroDesktop/ItemFiltro.jsx
@@ -0,0 +1,191 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { RiArrowDownSLine } from "react-icons/ri";
+import { BsCheck2, BsX } from "react-icons/bs";
+import useFiltros from "../../../../hooks/useFiltros";
+
+const Item = styled.li`
+  width: 100%;
+  border-top: 1px solid #d1d1d1;
+  border-bottom: 1px solid #d1d1d1;
+`;
+const ContenidoItem = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .btn-drop {
+    display: block;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-right: 5px;
+    height: 42px;
+    text-align: left;
+    width: 100%;
+  }
+`;
+
+const DropdownItem = styled.div`
+  position: absolute;
+  width: 350px;
+  background: var(--color-gris);
+  display: none;
+  z-index: 2;
+  box-shadow: 5px 5px 5px #d4d4d465;
+  &.visible {
+    display: block;
+  }
+  &.oculto {
+    display: none;
+  }
+  header {
+    width: 100%;
+    padding: 15px 10px;
+    background: white;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    p {
+      white-space: nowrap;
+      font-size: 16px;
+    }
+    .btn-check {
+      padding: 10px;
+      text-transform: uppercase;
+      border: 1px solid var(--color-gris);
+      display: flex;
+      span {
+        display: block;
+        margin-left: 5px;
+      }
+    }
+  }
+  .seleccionados {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    span {
+      margin-top: 5px;
+      display: flex;
+    }
+    .item-seleccionado {
+      font-size: 12px;
+      margin-right: 10px;
+    }
+    &::after {
+      content: "";
+      width: 30%;
+      height: 100%;
+      background: linear-gradient(to right, #ffffff4c, #ffffff);
+      position: absolute;
+      right: 0;
+    }
+  }
+  .contenido-dropdown {
+    max-height: 300px;
+    overflow-y: scroll;
+    margin-top: 20px;
+    li {
+      background: white;
+      padding: 15px 10px;
+      margin: 10px;
+      color: #636363;
+      border-radius: 5px;
+      position: relative;
+    }
+    .seleccionado {
+      font-weight: 900;
+    }
+    .seleccionado:after {
+      content: "";
+      left: 0px;
+      bottom: 0px;
+      background: var(--color-principal);
+      position: absolute;
+      width: 5px;
+      height: 100%;
+      border-radius: 5px 0px 0px 5px;
+    }
+  }
+`;
+
+const ItemFiltro = ({ data }) => {
+  const [claseActiva, setClaseActiva] = useState();
+  const [currentItem, setCurrentItem] = useState();
+  const { changeSelection, changeSelectionAll, seleccionados } = useFiltros();
+
+  const handleClick = (data) => {
+    setCurrentItem(data);
+    setClaseActiva(data.id);
+  };
+
+  return (
+    <Item
+      onClick={() => {
+        handleClick(data);
+      }}
+      onMouseLeave={() => {
+        setClaseActiva(null);
+      }}
+    >
+      <ContenidoItem>
+        <button className="btn-drop">
+          {data.name} <RiArrowDownSLine></RiArrowDownSLine>
+        </button>
+        <DropdownItem className={data.id == claseActiva ? "visible" : "oculto"}>
+          <header>
+            <div className="seleccionados">
+              <p>{seleccionados.length} seleccionados</p>
+              <span className="seleccionados">
+                {seleccionados.map((seleccionado) => {
+                  return (
+                    <p className="item-seleccionado" key={seleccionado.id}>
+                      {seleccionado.name},
+                    </p>
+                  );
+                })}
+              </span>
+            </div>
+            {data?.facetValues.some((obj) => obj.isSelected == true) ? (
+              <button
+                className="btn-check"
+                onClick={() => {
+                  changeSelectionAll(data, false);
+                }}
+              >
+                <BsX></BsX> <span>borrar</span>
+              </button>
+            ) : (
+              <button
+                className="btn-check"
+                onClick={() => {
+                  changeSelectionAll(data, true);
+                }}
+              >
+                <BsCheck2></BsCheck2> <span>todo</span>
+              </button>
+            )}
+          </header>
+          <ul className="contenido-dropdown">
+            {currentItem?.facetValues.map((value) => {
+              return (
+                <li
+                  className={value.isSelected && "seleccionado"}
+                  key={value.id}
+                  onClick={() => {
+                    changeSelection(value);
+                  }}
+                >
+                  {value.name}
+                </li>
+              );
+            })}
+          </ul>
+        </DropdownItem>
+      </ContenidoItem>
+    </Item>
+  );
+};
+
+export default ItemFiltro;
