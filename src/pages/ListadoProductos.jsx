aba0998d7d50208147c853a766bf09211203cdde
Diego Aldao <diego_ind@live.com.ar>
2023-03-28 19:50:21 -0300

    agregado el contexto de categorias y añadidos props a los componentes

--- src/pages/ListadoProductos.jsx
+++ src/pages/ListadoProductos.jsx
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../components/Layout";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navegacion from "../components/ListadoProductos/Navegacion";
 import FiltroMovil from "../components/ListadoProductos/Filtros/FiltroMovil/FiltroMovil";
 import BotonFiltroMovil from "../components/ListadoProductos/Filtros/FiltroMovil/BotonFiltroMovil";
-import dataProductos from "../data/productosEsp.json";
+import dataProductos from "../data/productosMujer.json";
 import Productos from "../components/ListadoProductos/Productos";
 import FiltroDesktop from "../components/ListadoProductos/Filtros/FiltroDesktop/FiltroDesktop";
 import Header from "../components/ListadoProductos/Header";
+import CategoriaContext from "../context/IdCategoriaContext";
+
 const ListadoProductos = () => {
-  let { idCategoria, categoria2 } = useParams();
+  let { genero, categoria1, categoria2, idCategoria } = useParams();
   const [isOpen, setIsOpen] = useState();
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
   const [filtros, setFiltros] = useState();
+  const { setCategoria } = useContext(CategoriaContext);
 
   useEffect(() => {
     setData(dataProductos);
     setFiltros(dataProductos.facets);
     setLoading(false);
+    setCategoria(idCategoria);
   }, []);
 
   return (
     <Layout>
-      <Navegacion />
-      <Header nombre={categoria2} />
+      <Navegacion
+        genero={genero}
+        categoria1={categoria1}
+        categoria2={categoria2}
+      />
+      <Header nombre={categoria2 ? categoria2 : categoria1} />
       <BotonFiltroMovil isOpen={isOpen} setIsOpen={setIsOpen} />
       <FiltroMovil
         isOpen={isOpen}
@@ -35,7 +43,7 @@ const ListadoProductos = () => {
         loading={loading}
       />
       <FiltroDesktop data={filtros} />
-      <Productos data={data} />
+      <Productos data={data} idCategoria={idCategoria} />
     </Layout>
   );
 };
