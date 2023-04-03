242b344ca775725d698a0e6231e6feea006d4adb
Diego Aldao <diego_ind@live.com.ar>
2023-04-03 19:07:02 -0300

    añadidas las paginas de carrito y guardados

---
+++ src/pages/CarritoProductos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Carrito from "../components/Carrito/Carrito";
+const CarritoProductos = () => {
+  return (
+    <Layout>
+      <Carrito />
+    </Layout>
+  );
+};
+
+export default CarritoProductos;
---
+++ src/pages/ProductosGuardados.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Guardados from "../components/Guardados/Guardados";
+
+const ProductosGuardados = () => {
+  return (
+    <Layout>
+      <Guardados />
+    </Layout>
+  );
+};
+
+export default ProductosGuardados;
