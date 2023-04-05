8feba8a0d3201b2cd8ede36628f345157ed13165
Diego Aldao <diego_ind@live.com.ar>
2023-04-05 18:05:48 -0300

    añadidas las paginas de login y registro

---
+++ src/pages/LoginUsuario.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutUsuario from "../Layout/LayoutUsuario";
+import Login from "../components/Usuario/Login/Login";
+
+const LoginUsuario = () => {
+  return (
+    <LayoutUsuario>
+      <Login />
+    </LayoutUsuario>
+  );
+};
+
+export default LoginUsuario;
---
+++ src/pages/RegistroUsuario.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Registro from "../components/Usuario/Registro/Registro";
+import LayoutUsuario from "../Layout/LayoutUsuario";
+
+const RegistroUsuario = () => {
+  return (
+    <LayoutUsuario>
+      <Registro />
+    </LayoutUsuario>
+  );
+};
+
+export default RegistroUsuario;
