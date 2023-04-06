8066970c78b53dc06f5a520cf65c97c1ed6be8d1
Diego Aldao <diego_ind@live.com.ar>
2023-04-06 09:20:24 -0300

    terminados los estilos, agregado el link al inicio  y agregada una clase para cuando no hay preview de guardados

--- src/components/SinProductos.jsx
+++ src/components/SinProductos.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import PreviewGuardados from "./Carrito/PreviewGuardados";
+import { Link } from "react-router-dom";
 
 const ContenedorVacio = styled.div`
-  height: calc(100vh - 136px);
   position: relative;
   display: flex;
   flex: 1 0 auto;
-  min-height: 444px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   background: white;
-  @media (min-width: 480px) {
-    height: calc(100vh - 102px);
+
+  @media (min-width: 1440px) {
+    min-height: calc(100vh - 402px);
   }
-  @media (min-width: 768px) {
-    height: calc(100vh - 352px);
-  }
-  @media (min-width: 1024px) {
-    min-height: 402px;
-    height: calc(100vh - 402px);
+  &.vacio-completo {
+    min-height: 500px;
+    height: calc(100vh - 137px);
+    @media (min-width: 480px) {
+      height: calc(100vh - 102px);
+    }
+    @media (min-width: 768px) {
+      height: calc(100vh - 352px);
+    }
+    @media (min-width: 1024px) {
+      min-height: 402px;
+      height: calc(100vh - 402px);
+    }
   }
 `;
 
 const InfoVacio = styled.div`
+  padding: 90px 20px;
   align-items: center;
   display: flex;
   flex-direction: column;
@@ -32,7 +40,7 @@ const InfoVacio = styled.div`
   justify-content: center;
   text-align: center;
   width: 100%;
-  padding: 0px 20px;
+  max-width: 400px;
   svg {
     height: 30px;
     width: 30px;
@@ -50,26 +58,27 @@ const InfoVacio = styled.div`
   p:first-letter {
     text-transform: uppercase;
   }
-  button {
-    background: black;
-    width: 280px;
-    color: white;
-    text-transform: uppercase;
-    font-weight: 700;
-    padding: 18px 0px 16px;
-    font-size: 16px;
-  }
 `;
 
-const SinProductos = ({ data }) => {
+const BotonInicio = styled(Link)`
+  background: black;
+  width: 280px;
+  color: white;
+  text-transform: uppercase;
+  font-weight: 700;
+  padding: 18px 0px 16px;
+  font-size: 16px;
+`;
+
+const SinProductos = ({ data, clase }) => {
   let { icono, titulo, descripcion, boton, carrito } = data;
   return (
-    <ContenedorVacio>
+    <ContenedorVacio className={clase ? clase : ""}>
       <InfoVacio>
         <span>{icono}</span>
         <h3>{titulo}</h3>
         {descripcion && <p>{descripcion}</p>}
-        {boton && <button>volver al inicio</button>}
+        {boton && <BotonInicio to="/">volver al inicio</BotonInicio>}
       </InfoVacio>
       {carrito && !boton && <PreviewGuardados />}
     </ContenedorVacio>
