fa3aa6f8e4156506f7b2cb8d7a79bee79ce06f85
Diego Aldao <diego_ind@live.com.ar>
2023-04-18 20:21:52 -0300

    añadido una condicion a la data para que la cargue cuando sea true

--- src/components/DetalleProducto/Descripcion.jsx
+++ src/components/DetalleProducto/Descripcion.jsx
@@ -68,11 +68,11 @@ const Descripcion = ({ data }) => {
   };
 
   useEffect(() => {
-    ref.current.innerHTML = data.description;
-    ref2.current.innerHTML = data.brand.description;
-    ref3.current.innerHTML = data.info.sizeAndFit;
-    ref4.current.innerHTML = data.info.careInfo;
-    ref5.current.innerHTML = data.info.aboutMe;
+    ref.current.innerHTML = data?.description;
+    ref2.current.innerHTML = data?.brand.description;
+    ref3.current.innerHTML = data?.info.sizeAndFit;
+    ref4.current.innerHTML = data?.info.careInfo;
+    ref5.current.innerHTML = data?.info.aboutMe;
   }, []);
   return (
     <Detalle>
