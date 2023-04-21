b187ada6a3e7e81f3d80695632390094f24bcfec
Diego Aldao <diego_ind@live.com.ar>
2023-04-21 19:09:39 -0300

    corregidos los imports, y simplificado el conditional rendering, ahora es uno para todos los componentes. terminada la v1

--- src/pages/PaginaPrincipal.jsx
+++ src/pages/PaginaPrincipal.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import useWindowSize from "../hooks/useWindowSize";
 import LayoutPrincipal from "../Layout/LayoutPrincipal";
-import Header from "../components/Inicio/Header";
-import Hero from "../components/Inicio/Hero";
-import Descuentos from "../components/Inicio/Descuentos";
+import Header from "../components/Inicio/Header/Header";
+import Hero from "../components/Inicio/Hero/Hero";
+import Descuentos from "../components/Inicio/Descuentos/Descuentos";
 import UltimosLanzamientos from "../components/Inicio/UltimosLanzamientos/UltimosLanzamientos";
 import ItemLanzamientos from "../components/Inicio/UltimosLanzamientos/ItemLanzamientos";
 import Promos from "../components/Inicio/Promos/Promos";
@@ -12,54 +12,49 @@ import Tendencias from "../components/Inicio/Tendencias/Tendencias";
 import ItemTendencias from "../components/Inicio/Tendencias/ItemTendencias";
 import { useLocation } from "react-router-dom";
 import useContentHome from "../hooks/useContentHome";
+import Loading from "../components/Loading";
 
 const PaginaPrincipal = () => {
-  const size = useWindowSize(); //hook para obtener el width y height del window
-  const location = useLocation(); //hook de react-router-dom para obtener el pathname
+  const size = useWindowSize(); //custom hook para obtener el width y height del window
+  const location = useLocation();
   const genero = location.pathname;
-  const { dataGenero } = useContentHome({ genero }); //hook para obtener la data correcta segun el genero
+  const { dataGenero } = useContentHome({ genero }); //custom hook para obtener la data del genero correcto
 
   return (
     <LayoutPrincipal>
-      {size.width < 768 ? (
+      {dataGenero ? (
         <>
-          <Header />
-          <Hero data={dataGenero[0]} />
+          {size.width < 768 ? (
+            <>
+              <Header />
+              <Hero data={dataGenero.hero} />
+            </>
+          ) : (
+            <>
+              <Hero data={dataGenero.hero} />
+              <Header />
+            </>
+          )}
+          <UltimosLanzamientos>
+            {dataGenero.lanzamientos.children.map((item) => (
+              <ItemLanzamientos data={item} key={item.id} />
+            ))}
+          </UltimosLanzamientos>
+          <Descuentos data={dataGenero.descuento} />
+          <Promos>
+            {dataGenero.promos.children.map((item) => (
+              <ItemPromos data={item} key={item.id} />
+            ))}
+          </Promos>
+          <Tendencias>
+            {dataGenero.marcas.children.map((item) => (
+              <ItemTendencias data={item} key={item.id} />
+            ))}
+          </Tendencias>
         </>
       ) : (
-        <>
-          <Hero data={dataGenero[0]} />
-          <Header />
-        </>
+        <Loading />
       )}
-      <UltimosLanzamientos>
-        {dataGenero[1] !== undefined ? (
-          dataGenero[1].map((item) => {
-            return <ItemLanzamientos data={item} key={item.id} />;
-          })
-        ) : (
-          <>loading</>
-        )}
-      </UltimosLanzamientos>
-      <Descuentos data={dataGenero[2]} />
-      <Promos>
-        {dataGenero[3] !== undefined ? (
-          dataGenero[3].map((item) => {
-            return <ItemPromos data={item} key={item.id} />;
-          })
-        ) : (
-          <>loading</>
-        )}
-      </Promos>
-      <Tendencias>
-        {dataGenero[4] !== undefined ? (
-          dataGenero[4].map((item) => {
-            return <ItemTendencias data={item} key={item.id} />;
-          })
-        ) : (
-          <>loading</>
-        )}
-      </Tendencias>
     </LayoutPrincipal>
   );
 };
