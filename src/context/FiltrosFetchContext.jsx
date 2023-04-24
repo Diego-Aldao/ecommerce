56d5785b2f6d3db39657968ec5ef23b95c55a07e
Diego Aldao <diego_ind@live.com.ar>
2023-04-24 17:28:47 -0300

    añadido un objeto inicial para el filtrosFetch

--- src/context/FiltrosFetchContext.jsx
+++ src/context/FiltrosFetchContext.jsx
@@ -3,7 +3,16 @@ import React, { useState } from "react";
 const Context = React.createContext({});
 
 export const FiltrosFetchContextProvider = ({ children }) => {
-  const [filtrosFetch, setFiltrosFetch] = useState();
+  let initialParams = {
+    store: "ES",
+    limit: "48",
+    country: "ES",
+    currency: "EUR",
+    sizeSchema: "ES",
+    lang: "es-ES",
+    offset: "0",
+  };
+  const [filtrosFetch, setFiltrosFetch] = useState(initialParams);
   return (
     <Context.Provider value={{ filtrosFetch, setFiltrosFetch }}>
       {children}
