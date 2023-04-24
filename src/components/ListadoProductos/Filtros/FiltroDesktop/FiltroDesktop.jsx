8c12404d6714e9ccfa38dbdd8e612b21c3da6b26
Diego Aldao <diego_ind@live.com.ar>
2023-04-24 16:30:32 -0300

    terminados los estilos, los customs hooks useFiltros y useProductos, y añadido un condicional para cargar el contenido cuando loading sea false pero solo la primer vez que carga el componente, para mantener los mismos filtros aunq el loading sea true

--- src/components/ListadoProductos/Filtros/FiltroDesktop/FiltroDesktop.jsx
+++ src/components/ListadoProductos/Filtros/FiltroDesktop/FiltroDesktop.jsx
@@ -1,35 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import ContenedorWidth from "../../../../styles/ContenedorMaxWidth";
-import ItemFiltro from "./ItemFiltro";
+import ItemFiltro from "./ItemFiltro/ItemFiltro";
+import useFiltros from "../../../../hooks/useFiltros";
+import useProductos from "../../../../hooks/useProductos";
+import Loading from "../../../Loading";
 
 const Background = styled.div`
   width: 100%;
   background: var(--color-gris);
-`;
-
-const Contenedor = styled.ul`
-  width: 100%;
+  padding-bottom: 20px;
   display: none;
   @media (min-width: 768px) {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 15px;
-    padding: 20px 15px;
-    flex-wrap: wrap;
+    display: block;
   }
 `;
 
-const FiltroDesktop = ({ data }) => {
+const ContenedorFiltros = styled.ul`
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 15px;
+  padding: 20px 15px;
+  flex-wrap: wrap;
+`;
+const BotonAplicar = styled.button`
+  text-transform: uppercase;
+  margin: 10px auto 0px;
+  padding: 10px 20px;
+  border: 1px solid black;
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+const FiltroDesktop = ({
+  filtros,
+  currentItem,
+  setCurrentItem,
+  location,
+  idCategoria,
+  querys,
+}) => {
+  const { filtrosFetch, setFiltrosFetch } = useFiltros();
+  const { filterProductos } = useProductos();
+  const [currentLoading, setCurrentLoading] = useState(false);
+  //para hacer un loading solo la primera vez
+
+  const handleClick = () => {
+    //cada itemFiltro habra modificado el filtrosFetch y cuando haga este click, ya estara listo el objeto con los filtros para hacer el fetch
+    filterProductos(filtrosFetch);
+  };
+
+  useEffect(() => {
+    setCurrentLoading(true);
+    setTimeout(() => {
+      setCurrentLoading(false);
+    }, 4000);
+    if (location.pathname.includes("busqueda") && querys) {
+      setFiltrosFetch({ ...filtrosFetch, q: querys });
+    } else if (location.pathname.includes("productos") && idCategoria) {
+      setFiltrosFetch({ ...filtrosFetch, q: "", categoryId: idCategoria });
+    }
+  }, [idCategoria, querys]);
+
   return (
     <Background>
-      <ContenedorWidth>
-        <Contenedor>
-          {data?.map((item) => {
-            return <ItemFiltro key={item.id} data={item} />;
-          })}
-        </Contenedor>
-      </ContenedorWidth>
+      <>
+        {currentLoading ? (
+          <Loading maxHeight={"210px"} />
+        ) : (
+          <ContenedorFiltros>
+            {filtros?.map((item) => {
+              return (
+                <ItemFiltro
+                  key={item.id}
+                  data={item}
+                  currentItem={currentItem}
+                  setCurrentItem={setCurrentItem}
+                />
+              );
+            })}
+          </ContenedorFiltros>
+        )}
+
+        <BotonAplicar onClick={handleClick}>aplicar filtros</BotonAplicar>
+      </>
     </Background>
   );
 };
