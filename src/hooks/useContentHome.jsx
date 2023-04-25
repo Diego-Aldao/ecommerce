8270d7b4b2b1c0c0b60a942e7713d0359876739b
Diego Aldao <diego_ind@live.com.ar>
2023-04-25 11:49:22 -0300

    corregida la data que retorna el hook

--- src/hooks/useContentHome.jsx
+++ src/hooks/useContentHome.jsx
@@ -1,19 +1,14 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import dataHome from "../data/Home.json";
 
 const useContentHome = ({ genero }) => {
-  const [dataGenero, setDataGenero] = useState([]);
+  const [dataGenero, setDataGenero] = useState();
 
   const currentData =
     genero == "/hombre" ? dataHome.home[1].children : dataHome.home[0].children;
-  const hero = currentData[0];
-  const ultimosLanzamientos = currentData[1].children;
-  const descuento = currentData[2];
-  const promos = currentData[3].children;
-  const tendencias = currentData[4].children;
 
   useEffect(() => {
-    setDataGenero([hero, ultimosLanzamientos, descuento, promos, tendencias]);
+    setDataGenero(currentData);
   }, [genero]);
 
   return { dataGenero };
