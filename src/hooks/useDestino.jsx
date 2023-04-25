5e72281d9e18a9db23af36a577c63c64e79232a8
Diego Aldao <diego_ind@live.com.ar>
2023-04-25 11:56:47 -0300

    añadidos condicionales y un replace all para los ctas

--- src/hooks/useDestino.jsx
+++ src/hooks/useDestino.jsx
@@ -5,16 +5,16 @@ const useDestino = (link) => {
   const [linkFormateado, setLinkFormateado] = useState("");
 
   useEffect(() => {
-    if (link.includes("/")) {
-      let ruta = link
-        .replaceAll("https://www.asos.com/es/", "/")
-        .replaceAll("/cat", "")
-        .split("?", 2);
-      let url = ruta[0];
-      let querys = ruta[1].replaceAll("cid=", "categoryId=");
-      const destino = `/productos${url}${querys}`;
-      setLinkFormateado(destino);
-    }
+    if (!link.includes("/")) return;
+    let ruta = link
+      .replaceAll("https://www.asos.com/es/", "/")
+      .replaceAll("/cat", "")
+      .replaceAll("/ctas", "")
+      .split("?", 2);
+    let url = ruta[0] ? ruta[0] : ruta;
+    let querys = ruta[1]?.replaceAll("cid=", "categoryId=");
+    const destino = `/productos${url}${querys}`;
+    setLinkFormateado(destino);
   }, [link]);
   return { linkFormateado };
 };
