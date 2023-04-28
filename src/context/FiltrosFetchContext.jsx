03430f8bc9c6c949c1a1356ff79fe6702e1ac771
Diego Aldao <diego_ind@live.com.ar>
2023-04-28 16:32:03 -0300

    corregido el orden de las keys

--- src/context/FiltrosFetchContext.jsx
+++ src/context/FiltrosFetchContext.jsx
@@ -5,13 +5,14 @@ const Context = React.createContext({});
 export const FiltrosFetchContextProvider = ({ children }) => {
   let initialParams = {
     store: "ES",
+    offset: "0",
     limit: "48",
     country: "ES",
     currency: "EUR",
     sizeSchema: "ES",
     lang: "es-ES",
-    offset: "0",
   };
+
   const [filtrosFetch, setFiltrosFetch] = useState(initialParams);
   return (
     <Context.Provider value={{ filtrosFetch, setFiltrosFetch }}>
